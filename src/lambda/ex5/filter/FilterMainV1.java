720663329b2d08bb8d192ac88e0f6be7d62aaa85
ehgk4245 <ehgkrk14@naver.com>
2025-07-24 11:18:54 +0900

    함수형 interface 연습

---
+++ src/lambda/ex5/filter/FilterMainV1.java
@@ -0,0 +1,38 @@
+package lambda.ex5.filter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilterMainV1 {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        List<Integer> evenNumbers = filterEvenNumber(numbers);
+        System.out.println("evenNumbers = " + evenNumbers);
+
+        List<Integer> oddNumbers = filterOddNumber(numbers);
+        System.out.println("oddNumbers = " + oddNumbers);
+    }
+
+    private static List<Integer> filterEvenNumber(List<Integer> numbers) {
+        List<Integer> filtered = new ArrayList<>();
+        for (int number : numbers) {
+            boolean isEven = number % 2 == 0;
+            if (isEven) {
+                filtered.add(number);
+            }
+        }
+        return filtered;
+    }
+
+    private static List<Integer> filterOddNumber(List<Integer> numbers) {
+        List<Integer> filtered = new ArrayList<>();
+        for (int number : numbers) {
+            boolean isOdd = number % 2 == 1;
+            if (isOdd) {
+                filtered.add(number);
+            }
+        }
+        return filtered;
+    }
+}
